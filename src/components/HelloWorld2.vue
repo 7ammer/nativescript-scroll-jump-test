65a9b285ed5d7f5c9d6afe2867976320a4c5023c
James Isbell <james.isbell@worthers.com>
2018-04-25 18:11:40 +0100

    first commit

---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,61 @@
+<template>
+	<Page class="page">
+		<ActionBar title="next" class="action-bar"/>
+		<ScrollView>
+			<StackLayout class="">
+				<StackLayout class="" backgroundColor="red">
+					<!--Add your page content here-->
+					<Label textWrap="true" text="Play with NativeScript!" class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Write code in the editor or drag and drop components to build a NativeScript mobile application."
+					       class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code."
+					       class="h2 description-label"/>
+				</StackLayout>
+				<StackLayout class="" backgroundColor="lime">
+					<!--Add your page content here-->
+					<Label textWrap="true" text="Play with NativeScript!" class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Write code in the editor or drag and drop components to build a NativeScript mobile application."
+					       class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code."
+					       class="h2 description-label"/>
+				</StackLayout>
+				<StackLayout class="" backgroundColor="red">
+					<!--Add your page content here-->
+					<Label textWrap="true" text="Play with NativeScript!" class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Write code in the editor or drag and drop components to build a NativeScript mobile application."
+					       class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code."
+					       class="h2 description-label"/>
+				</StackLayout>
+				<StackLayout class="" backgroundColor="lime">
+					<!--Add your page content here-->
+					<Label textWrap="true" text="Play with NativeScript!" class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Write code in the editor or drag and drop components to build a NativeScript mobile application."
+					       class="h2 description-label"/>
+					<Label textWrap="true"
+					       text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code."
+					       class="h2 description-label"/>
+				</StackLayout>
+			</StackLayout>
+		</ScrollView>
+	</Page>
+</template>
+
+
+<script>
+
+	export default {
+		data() {
+			return {
+				surprise: false,
+			};
+		},
+	};
+</script>
